89c7dab15362f3d3c3f0e334dfefeeedee563a63
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-07-19 16:51:40 +0300

    new function to validate form

--- js/script.js
+++ js/script.js
@@ -22,11 +22,27 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#product-image").click(function(){
-    $("#product-image").hide();
-    $("#product").show();
+    $("#product-image").slideDown('1000').hide();
+    $("#product").show('1000');
   });
   $("#product").click(function(){
-    $("#product").hide();
-    $("#product-image").show();
+    $("#product").slideUp('1000');
+    $("#product-image").slideDown('1000');
   });
+});
+
+$(document).ready(function(){
+  $("form#form34A").submit(function(event){
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#comment").val();
+    if ($("input#name").val() && $("input#email").val() && $("textarea#comment").val()!= "" ){
+      alert ("Thank you " + name + " We have recieved your message");
+    }
+    else {
+      alert("Please enter your name and email!")
+    }
+    event.preventDefault();
+  });
+
 });
