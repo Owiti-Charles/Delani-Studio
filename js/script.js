e10b5316cdf5288c4dd8376724dbfa1bb0d8778a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-07-21 22:03:47 +0300

    remove event.preventDefault of form submit

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("form#form34A").submit(function(event){
-    event.preventDefault();
+    // event.preventDefault();
     var name = $("input#MERGE1").val();
     var email = $("input#MERGE0").val();
     var message = $("textarea#comment").val();
