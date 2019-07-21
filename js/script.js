aa3c4a3a938d04e3012300421fb8c56ef9011e99
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-07-21 10:51:05 +0300

    Add mail chimp API to track user messages

--- js/script.js
+++ js/script.js
@@ -85,16 +85,16 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("form#form34A").submit(function(event){
-    var name = $("input#name").val();
-    var email = $("input#email").val();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
     var message = $("textarea#comment").val();
-    if ($("input#name").val() && $("input#email").val() && $("textarea#comment").val()!= "" ){
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
       alert (name + ", we have received your message. Thank you for reaching out to us.");
     }
     else {
-      alert("Please enter your name and email!")
+      alert("Please enter your name and email!");
     }
-    // event.preventDefault();
+    // event.preventDefault('https://gmail.us3.list-manage.com/subscribe/post');
   });
 
 });
